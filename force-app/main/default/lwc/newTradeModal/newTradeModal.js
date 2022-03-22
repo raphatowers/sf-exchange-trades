5642aa1240ef19e4d8303bf1b77556adf6496a3b
Rafael Torres <ratorres@financialforce.com>
2022-03-22 09:11:42 +0100

    UPDATE new trade modal to add picklists, rate and apex call to retrieve rates based on the selected currency

--- force-app/main/default/lwc/newTradeModal/newTradeModal.js
+++ force-app/main/default/lwc/newTradeModal/newTradeModal.js
@@ -1,13 +1,48 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 import { Close } from './events.js';
+import getExchangeRates from '@salesforce/apex/ExchangeTradeController.getExchangeRates';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+
 export default class NewTradeModal extends LightningElement {
-    
+    supportedCurrencies = [
+        { label: 'EUR', value: 'EUR' },
+        { label: 'USD', value: 'USD' },
+        { label: 'GBP', value: 'GBP' },
+    ];
+    buyCurrencies = [];
+    sellCurrency = '';
+    buyCurrency = '';
+    rate;
 
 	handleClose() {
 		this.dispatchEvent(new Close());
 	}
 
-    handleCreate() {
+    handleSellCurrencyChange(event) {
+        this.sellCurrency = event.target.value;
+        this.handleRates();
+    }
 
+    handleRates() {
+        getExchangeRates({
+            currencyCode: this.sellCurrency
+        })
+        .then((result) => {
+            this.buyCurrencies = result;
+        })
+        .catch((error) => {
+            this.dispatchEvent(
+				new ShowToastEvent({
+					message: error.body.message,
+					variant: 'error',
+                    mode: 'sticky',
+				})
+			);
+        });
+    }
+
+    handleBuyCurrencyChange(event) {
+        this.buyCurrency = event.target.value;
+        this.rate = this.buyCurrencies.find(currency => currency.value == this.buyCurrency).rate;
     }
 }
